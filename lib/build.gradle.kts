f232c56e1c858a58b3a3213a71f7dcd5f7d339cb
Adilson Pedroso <dilspedro@gmail.com>
2025-02-16 13:30:46 +0100

    Remove guava dependency

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -5,11 +5,8 @@ plugins {
 
 dependencies {
     testImplementation(libs.junit.jupiter)
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("io.minio:minio:8.5.17")
-
-    implementation(libs.guava)
 }
 
 application {
